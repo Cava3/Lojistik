e87f6189975f03ef525ec06d440b0acb844afdfb
LeMage <lucastag77@gmail.com>
2022-12-29 17:58:00 +0000

    Ajout du fonctionnement des modals

---
+++ modals.js
@@ -0,0 +1,29 @@
+// Logique généraliste des modales + définition des balises <modal> et <close>
+
+var actModal = null;
+
+function openModal(id) {
+    if(actModal){
+        actModal.style.display = 'none';
+    }
+    actModal = document.querySelector("modal#"+id);
+    actModal.style.display = 'block';
+}
+
+function closeModal() {
+    if(actModal){
+        actModal.style.display = 'none';
+        actModal = null;
+    }
+}
+
+const close_buttons = document.querySelectorAll('close');
+close_buttons.forEach((button) => {
+    button.onclick = closeModal;
+});
+
+window.onclick = function(event) {
+    if (event.target == actModal) {
+        closeModal();
+    }
+}
---
+++ script.js
@@ -0,0 +1,7 @@
+// Association des modals
+document.getElementById("import-btn").onclick = function() {
+    openModal("import-modal");
+}
+document.getElementById("export-btn").onclick = function() {
+    openModal("export-modal");
+}
