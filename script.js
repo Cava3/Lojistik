38148b298f3859fd36dfc50256619b5c71fcdf63
LeMage <lucastag77@gmail.com>
2023-01-02 09:39:27 +0000

    Back de copy-paste + stockage dico

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var dico_values = {};
+var dico_rows = {};
+var table = document.getElementById("key-pasta-copy-delete");
+
 // Association des modals
 document.getElementById("import-btn").onclick = function() {
     openModal("import-modal");
@@ -5,28 +9,69 @@ document.getElementById("import-btn").onclick = function() {
 document.getElementById("export-btn").onclick = function() {
     openModal("export-modal");
 }
+document.getElementById("values-btn").onclick = function() {
+    openModal("values-modal");
+}
+document.getElementById("info-btn").onclick = function() {
+    openModal("info-modal");
+}
 
+// Fonctions d'ajout/supression de lignes
+function addFromForm() {
+    var keyForm = document.getElementById("add-key");
+    var valueForm = document.getElementById("add-value");
+
+    if(keyForm.value && valueForm.value){
+        addRow(keyForm.value, valueForm.value);
+        keyForm.value = "";
+        valueForm.value = "";
+    } else {
+        alert("Veuillez remplir les champs.");
+    }
+}
+
+function addRow(key, value) {
+    dico_values[key] = value;
+
+    var raw_row = 
+        "<td>"+key+"</td>"+
+        "<td>"+value+"</td>"+
+        "<td><button onclick='navigator.clipboard.writeText(\""+value+"\")'>🧾</button></td>"+
+        "<td><button onclick='removeRow(\""+key+"\")'>🗑️</button></td>";
+
+    var row = document.createElement("tr");
+    row.innerHTML = raw_row;
+    table.appendChild(row);
+
+    dico_rows[key] = row;
+}
+
+function removeRow(key) {
+    table.removeChild(dico_rows[key]);
+
+    delete dico_values[key];
+    delete dico_rows[key];
+}
 
 // Recording button
 var recording = false;
-var recordButton = document.getElementById("toggle-speech");
+var record_button = document.getElementById("toggle-speech");
 
 function toggleRecording() {
-    if (recording) {
+    if (recording)
         stopRecording();
-    } else {
+    else
         startRecording();
-    }
 }
 
 // Recording functions
 function startRecording() {
     if(!recognition){
-        console.log("Speech recognition not supported");
+        alert("Votre navigateur ne supporte pas la reconnaissance vocale.");
         return;
     }
 
-    recordButton.classList="on";
+    record_button.classList="on";
     console.log("start recording");
     
     recording = true;
@@ -34,7 +79,7 @@ function startRecording() {
 }
 
 function stopRecording() {
-    recordButton.classList="off";
+    record_button.classList="off";
     console.log("stop recording");
     
     recording = false;
